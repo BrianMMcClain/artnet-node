f599763318b7e663a434268ae68479dabd072ce1
Brian McClain <brianmmcclain@gmail.com>
2012-01-18 23:59:25 -0500

    Shuffled around logic, still trying to figure out proper patterns for writing Node modules

--- test/test-server.js
+++ test/test-server.js
@@ -1,4 +1,4 @@
 var sys = require('util');
-var artnet = require('../lib/artnet');
+var artnetsrv = require('../lib/ArtNetServer');
 
-var srv = artnet.ArtNetServer();
+var srv = artnetsrv.listen('127.0.0.1', 6465);

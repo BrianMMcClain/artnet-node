21f84c8207c606fdacdf5125acdd6d0a50b5d89c
Brian McClain <brianmmcclain@gmail.com>
2012-01-23 23:33:51 -0500

    Client now automatically calculates length of data to send

--- test/test-server.js
+++ test/test-server.js
@@ -1,5 +1,5 @@
 var artnetsrv = require('../lib/artnet_server');
 
 var srv = artnetsrv.listen('127.0.0.1', 6454, function(msg, peer) {
-	console.log(msg.data);
+	console.log("Received a data packet of length " + msg.length + ": " + msg.data);
 });

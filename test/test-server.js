6a8eff4eacfb37a86c864ee991ac62f6d5d228d5
Brian McClain <brianmmcclain@gmail.com>
2012-02-07 21:01:08 -0500

    Removed need for hostname in artnet server

--- test/test-server.js
+++ test/test-server.js
@@ -1,6 +1,6 @@
 var artnetsrv = require('../lib/artnet_server');
 
-var srv = artnetsrv.listen('127.0.0.1', 6454, function(msg, peer) {
+var srv = artnetsrv.listen(6454, function(msg, peer) {
 	console.log("-----------------");
 	console.log("Sequence: " + msg.sequence);
 	console.log("Physical: " + msg.physical);
